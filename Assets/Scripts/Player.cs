d6b2b48c2a55c639510158a21a3e87434a080016
okonys <yoonsg1007@gmail.com>
2023-10-19 12:49:37 +0900

    Add PlayerMove, Add Asset
    움직이는 스크립트 추가, 에셋 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -8,26 +9,60 @@ public class Player : MonoBehaviour
     public Vector2 inputVec;
     public float speed;
     Rigidbody2D rigid;
+    Transform transform;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
     }
 
     void Start()
     {
         
     }
-
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
+    
     
     void FixedUpdate()
     {
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void Update()
+    {
+        //inputVec.x = Input.GetAxisRaw("Horizontal");
+        //inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void LateUpdate()
+    {
+        if(inputVec.x != 0 || inputVec.y != 0)
+        {
+            anim.SetFloat("RunState",0.35f);
+        }
+        else
+            anim.SetFloat("RunState",0f);
+
+        if(inputVec.x < 0 )
+        {
+            transform.localScale = new Vector3(1,transform.localScale.y,transform.localScale.z);
+        }
+        else if(inputVec.x > 0)
+        {
+            transform.localScale = new Vector3(-1,transform.localScale.y,transform.localScale.z);
+        }
+
+        if(Input.GetKeyUp(KeyCode.Z))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
     }
 }
